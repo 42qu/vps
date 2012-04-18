7350f3e96ee3f860a58ebe307037224bb92e887c
zuroc <zsp007@gmail.com>
2012-04-18 22:31:24 +0800

    fix

--- saas/gen-py/zthrift/server.py
+++ saas/gen-py/zthrift/server.py
@@ -1,7 +1,6 @@
 #coding:utf-8
 from config import SSL_KEY_PEM , SAAS_PORT, SAAS_HOST
 
-from thrift.transport.TSSLSocket import TSSLServerSocket as  TServerSocket
 
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
@@ -11,7 +10,12 @@ from thrift.server import TServer
 def server(saas, handler):
     processor = saas.Processor(handler)
 
-    transport = TServerSocket(SAAS_HOST, SAAS_PORT, certfile=SSL_KEY_PEM)
+#    from thrift.transport.TSSLSocket import TSSLServerSocket as  TServerSocket
+#    transport = TServerSocket(SAAS_HOST, port=SAAS_PORT, certfile=SSL_KEY_PEM)
+    
+    from thrift.transport import TSocket 
+    transport = TSocket.TServerSocket(SAAS_HOST, port=SAAS_PORT)
+
     tfactory  = TTransport.TBufferedTransportFactory()
     pfactory  = TBinaryProtocol.TBinaryProtocolFactory()
 
