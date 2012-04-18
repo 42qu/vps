d27d67a1acddf8101646a50419c228a2eadc74f8
zuroc <zsp007@gmail.com>
2012-04-18 21:24:39 +0800

    fix

---
+++ saas/gen-py/client/vps.py
@@ -0,0 +1,23 @@
+#coding:utf-8
+
+from saas.ttypes import Action
+
+class Handler(object):
+    def to_do(self, pc):
+        pass
+
+    def info(self, id):
+        pass
+
+    def opened(self, id):
+        pass
+
+    def closed(self, id):
+        pass
+
+    def restart(self, id):
+        pass
+
+if __name__ == "__main__":
+    pass
+
--- saas/gen-py/server_vps.py
+++ saas/gen-py/server_vps.py
@@ -3,47 +3,32 @@
 
 from config import SSL_KEY_PEM , SAAS_PORT
 
-
-from saas import VPS
-from saas.ttypes import Action
-
-
 from thrift.transport.TSSLSocket import TSSLServerSocket as  TServerSocket
 
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 from thrift.server import TServer
 
-class Handler(object):
-    def to_do(self, pc):
-        pass
 
-    def info(self, id):
-        pass
+def serve(saas, handler):
+    processor = saas.Processor(handler)
 
-    def opened(self, id):
-        pass
+    transport = TServerSocket(port=SAAS_PORT, certfile=SSL_KEY_PEM)
+    tfactory  = TTransport.TBufferedTransportFactory()
+    pfactory  = TBinaryProtocol.TBinaryProtocolFactory()
 
-    def closed(self, id):
-        pass
+    server    = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
 
-    def restart(self, id):
-        pass
+    # server = TServer.TThreadedServer(processor, transport, tfactory, pfactory)
+    # server = TServer.TThreadPoolServer(processor, transport, tfactory, pfactory)
+    server.serve()
 
 
-transport = TServerSocket(port=SAAS_PORT, certfile=SSL_KEY_PEM)
-tfactory  = TTransport.TBufferedTransportFactory()
-pfactory  = TBinaryProtocol.TBinaryProtocolFactory()
-
-handler   = Handler()
-processor = VPS.Processor(handler)
-server    = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
-
-# server = TServer.TThreadedServer(processor, transport, tfactory, pfactory)
-# server = TServer.TThreadPoolServer(processor, transport, tfactory, pfactory)
+from saas import VPS
+from client.vps import Handler
 
 print 'serving ...'
-server.serve()
+serve(VPS, Handler)
 print 'done'
 
 
