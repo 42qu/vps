818c0f163265441408f392be934d14f93c64ab68
zuroc <zsp007@gmail.com>
2012-04-18 23:00:53 +0800

    fix

--- saas/gen-py/client/vps.py
+++ saas/gen-py/client/vps.py
@@ -2,11 +2,11 @@
 
 from saas.ttypes import Action
 from time import sleep
-
+from config import HOST_ID
 
 def handler(client):
     while True:
-        action, id = client.to_do()
+        action, id = client.to_do(HOST_ID)
         if action:
             print action , id
         sleep(10)
