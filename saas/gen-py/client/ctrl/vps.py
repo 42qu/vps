24f5a93aea94980804518693082d7c5ec67e588f
zuroc <zsp007@gmail.com>
2012-04-19 00:08:39 +0800

    fix

--- saas/gen-py/client/ctrl/vps.py
+++ saas/gen-py/client/ctrl/vps.py
@@ -4,12 +4,36 @@ from saas.ttypes import Action
 from time import sleep
 from config import HOST_ID
 
+ACTION = {}
+def action(id):
+    def _(func):
+        ACTION[id] = func
+        return func
+    return _
+
+
+@action(Action.OPEN)
+def _open(client, id):
+    print id
+
+@action(Action.CLOSE)
+def _close(client, id):
+    print id
+
+@action(Action.RESTART)
+def _close(client, id):
+    print id
+
+
+
 def handler(client):
     while True:
         todo = client.todo(HOST_ID)
-        if todo.action:
-            pass 
-        print todo.action , todo.id
+        action = todo.action
+        if action:
+            func = ACTION.get(todo.action, 0)
+            if func:
+                func(client, id)
         sleep(10)
 
 
