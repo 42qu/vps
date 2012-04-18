0ebb00f293e83f483a1df1db0612a1f28c716167
zuroc <zsp007@gmail.com>
2012-04-18 20:05:45 +0800

    fix

--- saas/gen-py/config/__init__.py
+++ saas/gen-py/config/__init__.py
@@ -1,11 +1,2 @@
 import _env
 
-from os.path import dirname, normpath, abspath, join
-
-PREFIX = dirname(dirname(abspath(__file__)))
-
-SSL_KEY_PEM = join(dirname(PREFIX),'private/key.pem')
-
-SAAS_PORT = 50042
-
-from socket import gethostname
