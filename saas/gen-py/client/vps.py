9183fa94f1c062ea5867cf7090bd232b703d432e
zuroc <zsp007@gmail.com>
2012-04-18 22:32:15 +0800

    fix

--- saas/gen-py/client/vps.py
+++ saas/gen-py/client/vps.py
@@ -1,9 +1,13 @@
 #coding:utf-8
 
 from saas.ttypes import Action
+from time import sleep
 
 def handler(client):
-    print "!!!"
+    while Ture:
+        sleep(10)
+        print 1
+
 
 if __name__ == "__main__":
     pass
