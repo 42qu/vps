55ed8557eaa19d265553cc96a97aa4d97dc9aba1
zuroc <zsp007@gmail.com>
2012-04-18 23:22:01 +0800

    fix

---
+++ saas/gen-py/config/default.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def prepare(o):
+    from os.path import join, dirname
+    from config import PREFIX
+    
+    o.SSL_KEY_PEM = join(dirname(PREFIX),'private/key.pem')
+    o.SAAS_PORT = 50042
+
+    o.SAAS_HOST = "saas-vps.42qu.us"
+
+    return o
+
+
+def finish(o):
+    return o
+
