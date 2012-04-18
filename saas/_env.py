95ec0ae2024e0796ab8d929560a03ace1e0a1789
zuroc <zsp007@gmail.com>
2012-04-18 19:16:09 +0800

    fix

--- saas/_env.py
+++ saas/_env.py
@@ -5,6 +5,7 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 import sys
+from os.path import dirname, normpath, abspath, join
+sys.path.append(normpath(join(dirname(abspath(__file__)),"gen-py")))
 
- normpath(dirname(dirname(abspath(__file__))))
 
