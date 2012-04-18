4d0fcb2caccd2e4d295b9c69174432f383b31e9c
zuroc <zsp007@gmail.com>
2012-04-18 23:39:12 +0800

    fix

--- saas/gen-py/client/vps.dev.py
+++ saas/gen-py/client/vps.dev.py
@@ -2,6 +2,6 @@
 #coding:utf-8
 
 
+from vps import main
 from zkit.reloader.reload_server import auto_reload
-from client_vps import main
 auto_reload(main)
--- saas/gen-py/client/vps.py
+++ saas/gen-py/client/vps.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #coding:utf-8
-
+import _env
 
 from zthrift.client import client
 
