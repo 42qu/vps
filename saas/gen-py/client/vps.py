a59a1373640956ac9457f7aee79f2ffe55dcb189
zuroc <zsp007@gmail.com>
2012-04-18 22:14:10 +0800

    fix

--- saas/gen-py/client/vps.py
+++ saas/gen-py/client/vps.py
@@ -2,21 +2,8 @@
 
 from saas.ttypes import Action
 
-class Handler(object):
-    def to_do(self, pc):
-        pass
-
-    def info(self, id):
-        pass
-
-    def opened(self, id):
-        pass
-
-    def closed(self, id):
-        pass
-
-    def restart(self, id):
-        pass
+def handler(client):
+    print "!!!"
 
 if __name__ == "__main__":
     pass
