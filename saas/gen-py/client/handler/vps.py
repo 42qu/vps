308cdd8c6ddc583fcdd9ad16034a096190692faa
zuroc <zsp007@gmail.com>
2012-04-18 23:29:46 +0800

    fix

---
+++ saas/gen-py/client/handler/vps.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+
+from saas.ttypes import Action
+from time import sleep
+from config import HOST_ID
+
+def handler(client):
+    while True:
+        todo = client.todo(HOST_ID)
+        if todo.action:
+            pass 
+        print todo.action , todo.id
+        sleep(10)
+
+
+if __name__ == "__main__":
+    pass
+
