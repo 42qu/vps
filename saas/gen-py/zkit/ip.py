12c53cd09759798a0b68c9acafc45b4503e90197
zuroc <zsp007@gmail.com>
2012-04-19 16:43:04 +0800

    fix

---
+++ saas/gen-py/zkit/ip.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+import socket
+import struct
+
+def ip2int(ip):
+    return struct.unpack("!I",socket.inet_aton(ip))[0]
+
+def int2ip(num):
+    return socket.inet_ntoa(struct.pack("!I",num))
+
+if __name__ == "__main__":
+    print int2ip(ip2int("127.0.123.249"))
+    print int2ip(ip2int("201.119.123.249"))
+
