18850fcfce2d5bacdb10c383878f79e58b8398a1
zuroc <zsp007@gmail.com>
2012-04-20 01:05:35 +0800

    fix

--- saas/gen-py/client/model/vps.py
+++ saas/gen-py/client/model/vps.py
@@ -5,5 +5,5 @@ def vps_open(vps):
 
 if '__main__' == __name__:
     from saas.ttypes import Vps
-    vps = Vps(ipv4_gateway=2013143905, ram=2048, cpu=1, ipv4_netmask=-16, host_id=2, password='k5chpa2n4mz2', os=21, id=28, hd=50, ipv4=2013143908)
+    Vps(ipv4_gateway=2013143905, ram=2048, cpu=1, ipv4_netmask=4294967280, host_id=2, password='k5chpa2n4mz2', os=10002, id=28, hd=50, ipv4=2013143908)    
     vps_open(vps)
