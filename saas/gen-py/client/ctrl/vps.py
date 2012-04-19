39028f75401f36821496e53d98ee26d274a6b8e2
zuroc <zsp007@gmail.com>
2012-04-20 00:45:11 +0800

    fix

--- saas/gen-py/client/ctrl/vps.py
+++ saas/gen-py/client/ctrl/vps.py
@@ -5,11 +5,9 @@ from conf import HOST_ID
 from saas.ttypes import Cmd
 from _route import route
 from _handler import Handler
+from client.model.vps import vps_open
 
 
-
-def vps_open(vps):
-    print vps
 
 @route(Cmd.OPEN)
 def _open(client, id):
@@ -27,8 +25,5 @@ def _restart(client, id):
 
 handler = Handler(route, HOST_ID)
 
-if '__main__' == __name__:
-    from saas.ttypes import Vps
-    vps = Vps(ipv4_gateway=2013143905, ram=2048, cpu=1, ipv4_netmask=-16, host_id=2, password='k5chpa2n4mz2', os=21, id=28, hd=50, ipv4=2013143908)
-    vps_open(vps)
-
+if __name__ == "__main__":
+    pass
