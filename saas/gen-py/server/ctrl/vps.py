fd23d1579dd13d906c7a47995a1a69b59cccc39c
zuroc <zsp007@gmail.com>
2012-04-19 12:59:52 +0800

    fix

--- saas/gen-py/server/ctrl/vps.py
+++ saas/gen-py/server/ctrl/vps.py
@@ -1,10 +1,19 @@
 #coding:utf-8
 import _env
 from saas.ttypes import Cmd, Task
+from model.vps_sell import REDIS_VPS_SAAS_CMD
+from model._db import redis
+from array import array
 
 class Handler(object):
     def todo(self, host_id):
-        return Task(Cmd.OPEN, 0)
+        key = REDIS_VPS_SAAS_CMD%host_id
+        t = redis.rpoplpush(key , key)
+        if t:
+            s = array('I')
+            s.fromstring(t)
+            return Task(*s)
+        return Task()
 
     def vps(self, vps_id):
         pass
@@ -14,5 +23,6 @@ class Handler(object):
 
 if __name__ == "__main__":
     pass
-    from model.vps_sell import REDIS_VPS_SAAS_CMD
     print REDIS_VPS_SAAS_CMD
+    handler = Handler()
+    print handler.todo(2)
