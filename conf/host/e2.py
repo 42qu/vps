71d24baca4cccb09c189ac8481a7c3259df19c37
plan <frostyplanet@gmail.com>
2012-04-30 09:44:50 +0800

    add host/e2 conf

---
+++ conf/host/e2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+from os.path import dirname
+
+def prepare(o):
+    o.HOST_ID = 2
+    base_dir = dirname (dirname (dirname (__file__)))
+    o.log_dir = os.path.join (base_dir, "log")
+
+    o.RUN_DIR = os.path.join (base_dir, "run")
+    o.OS_IMAGE_DIR = "/vps/images/"
+    o.VPS_IMAGE_DIR = "/vps"
+    o.VPS_SWAP_DIR = "/swp/"
+    o.XEN_BRIDGE = "xenbr0"
+
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 :
