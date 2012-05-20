035c60d77513d7437ad824bc9dcf95eb0cc340af
xcf <xcf@xiachufang.com>
2012-05-20 18:47:00 +0800

    fix

--- server/model/plot.py
+++ server/model/plot.py
@@ -4,30 +4,30 @@ from zsql.db import connection
 connection.THREAD_SAFE = False
 from zsql.db import sqlstore
 from config import MYSQL_PORT, MYSQL_HOST, MYSQL_MAIN, MYSQL_USER, MYSQL_PASSWD
-from _mysql_exceptions import ProgrammingError 
+from _mysql_exceptions import ProgrammingError
 from time import time
 
 SQLSTORE = sqlstore.SqlStore(MYSQL_HOST , MYSQL_PORT, MYSQL_USER, MYSQL_PASSWD, db='zplot', charset='utf8')
 
 PLOT_CID_VPS_NETFLOW_RX_1MIN ,\
-PLOT_CID_VPS_NETFLOW_TX_1MIN, \
+PLOT_CID_VPS_NETFLOW_TX_1MIN,\
 PLOT_CID_VPS_NETFLOW_1MIN ,\
-PLOT_CID_VPS_NETFLOW_1MIN_HOST, \
+PLOT_CID_VPS_NETFLOW_1MIN_HOST,\
 PLOT_CID_VPS_NETFLOW_RX_1DAY ,\
-PLOT_CID_VPS_NETFLOW_TX_1DAY,  \
+PLOT_CID_VPS_NETFLOW_TX_1DAY,\
 PLOT_CID_VPS_NETFLOW_1DAY ,\
-PLOT_CID_VPS_NETFLOW_1DAY_HOST = range(1,9) 
+PLOT_CID_VPS_NETFLOW_1DAY_HOST = range(1, 9)
 
 CID2NAME = {
-    PLOT_CID_VPS_NETFLOW_RX_1MIN   : "VPS . 带宽图 . 流入 . 虚拟机. 每分钟",
-    PLOT_CID_VPS_NETFLOW_TX_1MIN   : "VPS . 带宽图 . 流出 . 虚拟机 . 每分钟",
-    PLOT_CID_VPS_NETFLOW_1MIN      : "VPS . 带宽图 . 虚拟机 . 每分钟",
-    PLOT_CID_VPS_NETFLOW_1MIN_HOST : "VPS . 带宽图 . 物理机 . 每分钟",
+    PLOT_CID_VPS_NETFLOW_RX_1MIN   : 'VPS . 带宽图 . 流入 . 虚拟机. 每分钟',
+    PLOT_CID_VPS_NETFLOW_TX_1MIN   : 'VPS . 带宽图 . 流出 . 虚拟机 . 每分钟',
+    PLOT_CID_VPS_NETFLOW_1MIN      : 'VPS . 带宽图 . 虚拟机 . 每分钟',
+    PLOT_CID_VPS_NETFLOW_1MIN_HOST : 'VPS . 带宽图 . 物理机 . 每分钟',
 
-    PLOT_CID_VPS_NETFLOW_RX_1DAY   : "VPS . 流量图 . 流入 . 虚拟机 . 每天",
-    PLOT_CID_VPS_NETFLOW_TX_1DAY   : "VPS . 流量图 . 流出 . 虚拟机 . 每天",
-    PLOT_CID_VPS_NETFLOW_1DAY      : "VPS . 流量图 . 虚拟机 . 每天",
-    PLOT_CID_VPS_NETFLOW_1DAY_HOST : "VPS . 流量图 . 物理机 . 每天",
+    PLOT_CID_VPS_NETFLOW_RX_1DAY   : 'VPS . 流量图 . 流入 . 虚拟机 . 每天',
+    PLOT_CID_VPS_NETFLOW_TX_1DAY   : 'VPS . 流量图 . 流出 . 虚拟机 . 每天',
+    PLOT_CID_VPS_NETFLOW_1DAY      : 'VPS . 流量图 . 虚拟机 . 每天',
+    PLOT_CID_VPS_NETFLOW_1DAY_HOST : 'VPS . 流量图 . 物理机 . 每天',
 }
 
 
@@ -36,7 +36,7 @@ def plot_point(cid, rid, limit, base=None):
     cursor.execute('select value from `%s` where rid=%%s order by id desc limit %s'%(int(cid), int(limit)), rid)
     if base is None:
         r = [i for i, in cursor.fetchall()]
-    else:    
+    else:
         r = [i/base for i, in cursor.fetchall()]
     return r
 
@@ -49,7 +49,7 @@ def plot( cid, rid, value):
     try:
         _plot( cid, rid, value, timestamp)
     except ProgrammingError, e:
-        if e.args and e.args[0]==1146:
+        if e.args and e.args[0] == 1146:
             cursor = SQLSTORE.cursor()
             cursor.execute(
 """
@@ -73,3 +73,4 @@ if __name__ == '__main__':
     print plot_point(PLOT_CID_VPS_NETFLOW_1MIN_HOST, 3, 1440, 1024*1024/8)
     print plot_point(PLOT_CID_VPS_NETFLOW_1MIN_HOST, 2, 1440, 1024*1024/8)
     print plot_point(PLOT_CID_VPS_NETFLOW_1MIN, 95, 1440, 1024*1024/8)
+    print plot_point(PLOT_CID_VPS_NETFLOW_1MIN, 14, 1440, 1024*1024/8)
