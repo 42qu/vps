e83b5ea615d4bb9aad5df06ac90d3edebf2d0dce
xcf <xcf@xiachufang.com>
2012-05-20 15:25:03 +0800

    fix

---
+++ server/model/plot.py
@@ -0,0 +1,64 @@
+#coding:utf-8
+import _env
+from zsql.db import connection
+connection.THREAD_SAFE = False
+from zsql.db import sqlstore
+from config import MYSQL_PORT, MYSQL_HOST, MYSQL_MAIN, MYSQL_USER, MYSQL_PASSWD
+from _mysql_exceptions import ProgrammingError 
+from time import time
+
+SQLSTORE = sqlstore.SqlStore(MYSQL_HOST , MYSQL_PORT, MYSQL_USER, MYSQL_PASSWD, db='zplot', charset='utf8')
+
+PLOT_CID_VPS_NETFLOW_RX_1MIN ,\
+PLOT_CID_VPS_NETFLOW_TX_1MIN, \
+PLOT_CID_VPS_NETFLOW_1MIN ,\
+PLOT_CID_VPS_NETFLOW_1MIN_HOST, \
+PLOT_CID_VPS_NETFLOW_RX_1DAY ,\
+PLOT_CID_VPS_NETFLOW_TX_1DAY,  \
+PLOT_CID_VPS_NETFLOW_1DAY ,\
+PLOT_CID_VPS_NETFLOW_1DAY_HOST = range(1,9) 
+
+CID2NAME = {
+    PLOT_CID_VPS_NETFLOW_RX_1MIN : "VPS . 带宽图 . 流入 . 虚拟机. 每分钟",
+    PLOT_CID_VPS_NETFLOW_TX_1MIN : "VPS . 带宽图 . 流出 . 虚拟机 . 每分钟",
+    PLOT_CID_VPS_NETFLOW_1MIN : "VPS . 带宽图 . 虚拟机 . 每分钟",
+    PLOT_CID_VPS_NETFLOW_1MIN_HOST : "VPS . 带宽图 . 物理机 . 每分钟",
+
+    PLOT_CID_VPS_NETFLOW_RX_1DAY : "VPS . 流量图 . 流入 . 虚拟机 . 每天",
+    PLOT_CID_VPS_NETFLOW_TX_1DAY : "VPS . 流量图 . 流出 . 虚拟机 . 每天",
+    PLOT_CID_VPS_NETFLOW_1DAY : "VPS . 流量图 . 虚拟机 . 每天",
+    PLOT_CID_VPS_NETFLOW_1DAY_HOST : "VPS . 流量图 . 物理机 . 每天",
+}
+
+
+def _plot( cid, rid, value, timestamp):
+    cursor = SQLSTORE.cursor()
+    cursor.execute('INSERT DELAYED INTO `%s` (rid, value, time) VALUES (%%s, %%s, %%s)'%int(cid), (rid, value, timestamp))
+
+def plot( cid, rid, value):
+    timestamp = int(time())
+    try:
+        _plot( cid, rid, value, timestamp)
+    except ProgrammingError, e:
+        if e.args and e.args[0]==1146:
+            cursor = SQLSTORE.cursor()
+            cursor.execute(
+"""
+CREATE TABLE  `zplot`.`%s` (
+`id` int(10) unsigned NOT NULL auto_increment,
+`value` bigint unsigned NOT NULL,
+`time` bigint unsigned NOT NULL,
+`rid` int(10) unsigned NOT NULL,
+PRIMARY KEY  (`id`),
+KEY `index_2` (`rid`)
+) ENGINE=MYISAM DEFAULT CHARSET=utf8;
+"""%cid
+
+            )
+            _plot(cid, rid, value, timestamp)
+        else:
+            raise e
+
+if __name__ == '__main__':
+    pass
+
