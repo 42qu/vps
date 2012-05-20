8f600884d5f3a713034d4c9c863dd58f6a955875
plan <frostyplanet@gmail.com>
2012-05-20 19:47:53 +0800

    add test/netflow.py

---
+++ test/netflow.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import _env
+import conf
+import saas
+import time
+from ops.netflow import read_proc
+from zthrift.client import get_client
+import unittest
+
+if __name__ == '__main__':
+    netflow_dict =  read_proc ()
+    print netflow_dict['vps95']
+
+
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 :
