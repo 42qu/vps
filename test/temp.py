c0922f19938f615359d2ff7f23289c8a11577c0f
plan <frostyplanet@gmail.com>
2012-07-23 17:19:24 +0800

    fix

---
+++ test/temp.py
@@ -0,0 +1,12 @@
+import sys
+import os
+import _env
+from vps_mgr import VPSMgr
+from saas.ttypes import Cmd
+
+if __name__ == '__main__':
+    client = VPSMgr ()
+#    client.done_task (Cmd.OPEN, 308, True)
+#    client.done_task (Cmd.OPEN, 309, True)
+
+
