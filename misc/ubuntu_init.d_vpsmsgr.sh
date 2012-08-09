a41391b9e06a43e5476993cd6be0a53014a994b0
Neptune Ning (Plan) <frostyplanet@gmail.com>
2012-08-09 16:21:21 +0800

    add ubuntu init

---
+++ misc/ubuntu_init.d_vpsmsgr.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+
+### BEGIN INIT INFO
+# Provides:             vpsmgr
+# Required-Start:       $xend
+# Required-Stop:        $xend
+# Default-Start:        2 3 4 5
+# Default-Stop:         
+# Short-Description:   vpsmgr 
+### END INIT INFO
+
+set -e
+
+
+BASE_PATH="/data/vps/code"
+cmd="./vps_mgr.py"
+
+function start() {
+    cd $BASE_PATH && $cmd start
+}
+
+function stop() {
+    cd $BASE_PATH && $cmd stop
+}
+
+function status() {
+    cd $BASE_PATH && $cmd status
+}
+
+function restart() {
+    stop
+    start
+}
+
+case "$1" in
+start)
+        start
+        ;;
+stop)
+        stop
+        ;;
+restart)
+        restart
+        ;;
+status)
+        status
+        ;;
+*)
+        echo $"Usage: $0 {start|stop|status|restart}"
+        RETVAL=2
+esac
+
+exit $RETVAL
+
+
