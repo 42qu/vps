f034254daf7d0c8f9d75730152e9495a9be06371
Neptune Ning (Plan) <frostyplanet@gmail.com>
2012-09-26 00:08:07 +0800

    migrate_vps support multiple vps

--- tools/migrate_vps.py
+++ tools/migrate_vps.py
@@ -21,7 +21,7 @@ def migrate_vps (vps_id, dest_ip):
         raise e
 
 def usage ():
-    print "usage: %s vps_id dest_ip" % (sys.argv[0])
+    print "usage: %s vps_id vps_id2 dest_ip" % (sys.argv[0])
 
 def main ():
     optlist, args = getopt.gnu_getopt (sys.argv[1:], "", [
@@ -33,12 +33,13 @@ def main ():
             usage ()
             os._exit (0)
     
-    if len (args) != 2:
+    if len (args) < 2:
         usage ()
         os._exit (1)
-    vps_id = int (args[0])
-    dest_ip = args[1]
-    migrate_vps (vps_id, dest_ip)
+    vps_ids = map (lambda x: int(x), args[0:-1])
+    dest_ip = args[-1]
+    for vps_id in vps_ids:
+        migrate_vps (vps_id, dest_ip)
 
 
 if "__main__" == __name__:
