5c5a480b978871cd34c6c507e623f557ca88d2cc
plan <frostyplanet@gmail.com>
2012-12-12 22:10:36 +0800

    add migrate_svr init script and update_host_list

---
+++ misc/init.d_vpsmigsvr.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+# vpsmgr      This shell script takes care of starting and stopping
+#                 vps manage daemon.
+#
+# chkconfig: - 13 87
+# description: vpsmigsvr.
+# probe: true
+
+. /etc/rc.d/init.d/functions
+
+BASE_PATH="/data/vps/code"
+cmd="./migrate_svr.py"
+
+export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/opt/bin"
+
+function start() {
+    cd $BASE_PATH && $cmd start
+}
+
+function stop() {
+    cd $BASE_PATH && $cmd stop
+}
+
+function status() {
+    cd $BASE_PATH && $cmd status
+}
+
+function restart() {
+    stop
+    start
+}
+
+case "$1" in
+start)
+        start
+        ;;
+stop)
+        stop
+        ;;
+restart)
+        restart
+        ;;
+status)
+        status
+        ;;
+*)
+        echo $"Usage: $0 {start|stop|status|restart}"
+        RETVAL=2
+esac
+
+exit $RETVAL
+
+
---
+++ tools/update_host_list.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+import os
+from os.path import dirname, abspath
+import sys
+import _env
+import conf
+from lib.log import Log
+import _saas
+import _saas.VPS
+from zthrift.client import get_client
+from zkit.ip import int2ip 
+
+def update_iplist (host_list):
+    conf_path = os.path.join (dirname (dirname (abspath (__file__))), "conf/private/migrate_svr.py")
+    ip_list = []
+    for host in host_list:
+        ip_list.append (int2ip(host.ext_ip))
+        ip_list.append (int2ip(host.int_ip))
+    conf_content = """#!/usr/bin/python
+
+ALLOWED_IPS = [
+%s
+]
+""" % (",\n".join (map (lambda x: "'%s'" % x, ip_list)))
+    f = open (conf_path, "w")
+    try:
+        f.write (conf_content)
+    finally:
+        f.close ()
+
+
+def main ():
+    logger = Log ("vps_mgr", config=conf)
+    trans, client = get_client (_saas.VPS)
+    host_list = None
+    try:
+        trans.open ()
+        try:
+            host_list = client.host_list ()
+        finally:
+            trans.close ()
+        update_iplist (host_list)
+    except Exception, e:
+        print e
+        logger.exception (e)
+        return
+       
+if __name__ == '__main__':
+    main ()
+    
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 :
