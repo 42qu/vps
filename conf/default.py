ef9013383c3509655fea0f2f584d8959208315b4
Adam Ning (Plan) <frostyplanet@gmail.com>
2013-03-10 12:46:31 +0800

    f

--- conf/default.py
+++ conf/default.py
@@ -53,7 +53,7 @@ def prepare(o):
     o.SAAS_BAD_THRESHOLD = 60 * 3
 
     o.DEFAULT_FS_TYPE = 'ext4'
-    o.CLOSE_EXPIRE_DAYS = 1
+    o.CLOSE_EXPIRE_DAYS = 0.8
     o.CGROUP_SCRIPT_DIR = "/data/vps/cgroup/"
     o.BLK_READ_IOPS = 1000
     o.BLK_WRITE_IOPS = 1000
