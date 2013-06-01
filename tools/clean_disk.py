e2d1d88bd1711012a52c9c80ed743afe6977854a
Adam Ning (Plan) <frostyplanet@gmail.com>
2013-06-01 19:56:24 +0800

    fix

--- tools/clean_disk.py
+++ tools/clean_disk.py
@@ -85,21 +85,24 @@ def _check_disk (client, vps_id, dry=True):
             vps_info = client.query_vps (vps_id)
         except Exception, e:
             raise e
-        if is_trash:
-            if not vps_info:
+        if not vps_info:
+            if is_trash:
                 _delete_all (logger, xv, "closed but no meta", dry=dry)
-            elif str(vps_info.host_id) != str(conf.HOST_ID):
+            else:
+                _delete_trash (logger, xv, "running but no meta", dry=dry)
+        elif vps_info.state == VM_STATE.CLOSE:
+            if str(vps_info.host_id) != str(conf.HOST_ID):
                 _delete_all (logger, xv, "closed but not on this host", dry=dry)
             else:
                 print "ignore closed vps", vps_id
                 return
-        else:
-            if not vps_info:
-                _delete_trash (logger, xv, "running but no meta", dry=dry)
+        elif vps_info.state == VM_STATE.OPEN:
             elif str(vps_info.host_id) != str(conf.HOST_ID):
                 _delete_all (logger, xv, "running but not on this host", days=7, dry=dry)
             else:
                 _delete_trash (logger, xv, "running", days=7, dry=dry)
+        elif vps_info.state == VM_STATE.RM:
+            _delete_all (logger, xv, "deleted", dry=dry)
 
 
 
@@ -110,7 +113,7 @@ def check_all (dry=True):
     client = VPSMgr ()
     configs = glob.glob (os.path.join (conf.VPS_METADATA_DIR, "*.json*"))
     for config in configs:
-        om = re.match (r'^vps(\d+)[\.\w]+$', os.path.basename (config))
+        om = re.match (r'^vps(\d+)\.[\.\w]+$', os.path.basename (config))
         if not om:
             continue
         vps_id = int(om.group (1))
